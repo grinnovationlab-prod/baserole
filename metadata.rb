3741341d6928584d7041e7292bf2749a38d2826e
Nic McElroy <nicamcelroy@gmail.com>
2018-05-11 17:16:18 +0100

    add metadata again..

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'baserole'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures baserole'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.1'
+depends          'chef-ingredient'
