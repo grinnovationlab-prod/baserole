68193f160587573044ba6dcd7c497ae63c8036af
Nic McElroy <nicamcelroy@gmail.com>
2018-06-01 17:17:00 +0100

    add service start and update version

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,7 +6,14 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+# Install the push jobs ingredient
 chef_ingredient 'push-jobs-client' do
     action :install
     version :latest
 end
+
+# make sure the created service is running
+windows_service 'push-jobs-client' do
+    action :start
+  end
