64f0ec3daef2b3744986a499eda31b48df6799dc
Nic McElroy <nicamcelroy@gmail.com>
2018-06-02 13:57:29 +0100

    seperate the depends

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,5 @@ license          'All rights reserved'
 description      'Installs/Configures baserole'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.5'
-depends          'chef-ingredient', 'audit'
+depends          'chef-ingredient'
+depends          'audit'
