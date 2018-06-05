246af286b139da854cb0e469fb2e1aa18af7d09f
Nic McElroy <nicamcelroy@gmail.com>
2018-06-05 17:28:31 +0100

    remove chef client updater as it wont work

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,8 +6,6 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-#ensure we are running the latest chef client
-chef_client_updater 'Install latest'
 
 # Setup the chef client in the way we want
 include_recipe 'chef-client::task'
